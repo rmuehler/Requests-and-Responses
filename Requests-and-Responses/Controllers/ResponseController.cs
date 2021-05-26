933bb9f770015f52a10276458c7d69ec8a928fb2
Robert Muehler <robertmuehler@gmail.com>
2021-05-26 09:45:38 -0400

    created web api, added het for headers

---
+++ Requests-and-Responses/Controllers/ResponseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Requests_and_Responses.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ResponseController : ControllerBase
+    {
+        [HttpGet("headers")]
+        public IActionResult Get()
+        {
+            return Ok(base.Request.Headers.ToList());
+        }
+        
+        
+        
+        
+    }
+}
